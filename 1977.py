7d8f16e6aa198094b08023e023c32da87974f962
Tickz0r <srtickdosgames@gmail.com>
2021-08-25 19:39:26 -0300

    print modos

--- 1977.py
+++ 1977.py
@@ -1,1 +1,9 @@
-print ('1977');
+print ('1977 - Iniciando Depuração!');
+
+#-------------#
+
+a = 9562
+b = 1541
+
+soma = a + b 
+print (soma)
---
+++ T1997.py
@@ -0,0 +1,18 @@
+a = 20
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+#-------print--------#
+
+print(type(soma))
+print('soma: ' + str (soma))
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(resto)
+#soma = str (soma)
+#print (type(soma))
