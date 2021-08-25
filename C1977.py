6d01e0c41112a6ff1c88a96d4e52c9c8640c35f5
Tickz0r <srtickdosgames@gmail.com>
2021-08-25 20:29:57 -0300

    Operadores lógicos

--- C1977.py
+++ C1977.py
@@ -1,2 +1,11 @@
-a = input ('Primeiro valor: ')
-b = input ('Segundo valor: ')
+a = int (input ('Primeiro valor: '))
+b = int (input ('Segundo valor: '))
+c = int (input ('Terceiro valor: '))
+
+if a > b and a > c:
+    print('O maior número é {}'.format(a))
+elif b > a and b > c:
+    print('o maior número é {}'.format(b))
+else:
+    print('o maior numero é {}'.format(c))
+print('Final da calculadora')
