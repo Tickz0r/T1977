1ced5f0aed16c0fa59f319fa1ba989f06acfc7f6
Tickz0r <srtickdosgames@gmail.com>
2021-08-26 08:02:36 -0300

    número par

---
+++ B1977.py
@@ -0,0 +1,9 @@
+a = int (input('Entre com o primeiro valor:'))
+b = int (input('Entre com o segundo valor:'))
+
+resto_a = a % 2
+resto_b = b % 2
+if resto_a == 0 or resto_b == 0:
+    print('foi digitado um número par')
+else:
+    print('nenhum número par foi digitado')
