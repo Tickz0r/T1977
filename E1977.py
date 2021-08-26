6e129abba148a43d5c6f6a1c6ba0f31d9568ca2c
Tickz0r <srtickdosgames@gmail.com>
2021-08-26 08:29:53 -0300

    Números Primos

---
+++ E1977.py
@@ -0,0 +1,16 @@
+a = int(input('Entre com o primeiro número: '))
+
+div = 0
+for x in range (1, a+1):
+    resto = a  % x
+    print (x, resto)
+    if resto == 0:
+        div = div + 1
+
+if div  == 2:
+    print('número {} é primo'.format(a))
+else:
+    print('número {} não é primo'.format(a))
+
+
+   
