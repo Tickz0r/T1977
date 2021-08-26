880684039076c211a31494c0ad82df0aab21405f
Tickz0r <srtickdosgames@gmail.com>
2021-08-26 08:09:12 -0300

    Contador de médias

--- B1977.py
+++ B1977.py
@@ -3,7 +3,7 @@ b = int (input('Entre com o segundo valor:'))
 
 resto_a = a % 2
 resto_b = b % 2
-if resto_a == 0 or resto_b == 0:
+if resto_a == 0 or not resto_b > 0:
     print('foi digitado um número par')
 else:
     print('nenhum número par foi digitado')
